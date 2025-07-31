18bd0e7e43dc77d7ff2b22c78095352972cf4cb8
RIDA RAFIQ <rafiqridaa@gmail.com>
2025-07-31 11:01:04 +0500

    Update first.c

--- c language/first.c
+++ c language/first.c
@@ -3,9 +3,16 @@ int main(){
     printf("hello world");
     return 0;
 }
-
+Task#01
 #include <stdio.h>
 int main(){
     printf("My name is RIDA RAFIQ and I love Cyber Security");
     return 0;
+}
+Task#02
+Identify and Label Each Part
+#include <stdio.h>         // [1] Preprocessor Directive – includes standard input/output library
+int main() {               // [2] Main Function – the starting point of the program
+    printf("This is a C program");   // [3] Output Statement – prints message to the screen
+    return 0;              // [4] Return Statement – ends the program successfully
 }
